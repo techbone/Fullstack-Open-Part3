2e647de64c0c9b2f33555b3f6e9553aea272e5f9
Musa Muhammad <musaawwaletudaye@gmail.com>
2021-08-19 12:14:47 +0100

    feat: Phonebook backend step3

--- index.js
+++ index.js
@@ -40,6 +40,17 @@ app.get("/api/info", (request, response) => {
   ${new Date()}`);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).send("Entry not found!");
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
